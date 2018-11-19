8e22fb413677c07aa5a51cc75a83824f24a1bd61
vi26 <44754162+vi26@users.noreply.github.com>
2018-11-19 23:50:24 +0300

    Задание 4.
    Теперь можно выбирать различные браузеры для тестов

--- java-example/src/test/java/ru/stqa/training/selenium/Task3AdminLoginTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/Task3AdminLoginTest.java
@@ -11,5 +11,5 @@ public class Task3AdminLoginTest extends TestBase {
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
-    }
+}
 }
--- java-example/src/test/java/ru/stqa/training/selenium/TestBase.java
+++ java-example/src/test/java/ru/stqa/training/selenium/TestBase.java
@@ -4,15 +4,29 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestBase {
     protected WebDriver driver;
     protected WebDriverWait wait;
+    String browser = BrowserType.FIREFOX;
+
 
     @Before
     public void start() {
-        driver = new ChromeDriver();
+        if (browser.equals(BrowserType.CHROME)) {
+            driver = new ChromeDriver();
+        } else if (browser.equals(BrowserType.FIREFOX)){
+            FirefoxOptions options = new FirefoxOptions().setLegacy(false);
+            driver = new FirefoxDriver(options);
+        } else{ if (browser.equals(BrowserType.EDGE))
+            driver = new EdgeDriver();
+
+        }
         wait = new WebDriverWait(driver, 10);
 
     }
