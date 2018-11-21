cf6e622d6553ff40b6f423cb51b33d0f224c1193
vi26 <44754162+vi26@users.noreply.github.com>
2018-11-22 00:07:27 +0300

    Реализовано прокликивиние элементов в задании 7.
    Прокликиваются последовательно все пункты меню слева, включая вложенные пункты.

--- java-example/src/test/java/ru/stqa/training/selenium/AdminLiteCart/Task7MoveToAllSectionsTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/AdminLiteCart/Task7MoveToAllSectionsTest.java
@@ -10,11 +10,21 @@ public class Task7MoveToAllSectionsTest extends TestBase {
 
     @Test
     public void testMoveToAllSection() {
-        int size = driver.findElements(By.cssSelector("li#app-")).size();
-        for (int i=0; i<size; i++) {
+        int numberOfWebElementsFromLeftPanel = driver.findElements(By.cssSelector("li#app-")).size();
+        for (int i=0; i<numberOfWebElementsFromLeftPanel; i++) {
             List <WebElement> arrayOfWebElements = driver.findElements(By.cssSelector("Li#app-"));
             arrayOfWebElements.get(i).click();
-            
+            //Проверка тега driver.findElements(By.cssSelector("h1"))
+            int numberOfIncludingWebElements = driver.findElements(By.cssSelector("[id ^= doc-]")).size();
+            if (numberOfIncludingWebElements != 0) {
+                for (int j=1; j<numberOfIncludingWebElements; j++){ // - 1 так как при открытии проверяется первый!!!!
+                    List <WebElement> arrayOfIncludingWebElements = driver.findElements(By.cssSelector("[id ^= doc-]"));
+                    arrayOfIncludingWebElements.get(j).click();
+                    //Проверка тега
+                }
+            }
+
+
 
         }
 
--- java-example/src/test/java/ru/stqa/training/selenium/AdminLiteCart/TestBase.java
+++ java-example/src/test/java/ru/stqa/training/selenium/AdminLiteCart/TestBase.java
@@ -14,7 +14,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class TestBase {
     protected WebDriver driver;
     protected WebDriverWait wait;
-    String browser = BrowserType.FIREFOX;
+    String browser = BrowserType.CHROME;
 
 
     @Before
