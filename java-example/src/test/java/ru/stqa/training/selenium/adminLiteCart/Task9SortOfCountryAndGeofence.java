0fe7396aa5fcf9bf324bb5ee255db0e07a7eab0b
vi26 <44754162+vi26@users.noreply.github.com>
2018-11-22 23:58:07 +0300

    Контрольная точка для задания 9

--- java-example/src/test/java/ru/stqa/training/selenium/adminLiteCart/Task9SortOfCountryAndGeofence.java
+++ java-example/src/test/java/ru/stqa/training/selenium/adminLiteCart/Task9SortOfCountryAndGeofence.java
@@ -15,13 +15,37 @@ public class Task9SortOfCountryAndGeofence extends TestBase {
 
     @Test
     public void testAdminLogin() {
-        driver.get(" http://localhost/litecart/admin/?app=countries&doc=countries");
-        List<String> listOfCountries = getListOfNameForCountries();
+       driver.get(" http://localhost/litecart/admin/?app=countries&doc=countries");
+       /* List<String> listOfCountries = getListOfNameForCountries();
         List<String> listOfSortCountries = new ArrayList<String>(listOfCountries);
         listOfSortCountries.sort(Comparator.<String, String>comparing(String::toString));
-        assertEquals(listOfCountries,listOfSortCountries);
+        assertEquals(listOfCountries,listOfSortCountries);*/
 
-        driver.findElements()
+        List<WebElement> webElements = driver.findElements(By.cssSelector("tr.row"));
+        int numberOfZones;
+        for (WebElement webElement : webElements) {
+
+            numberOfZones = Integer.parseInt(webElement.findElements(By.cssSelector("td")).get(5).getText());
+            if ( numberOfZones > 0) {
+                webElement.findElement(By.cssSelector("td a")).click();
+                List<String> listOfZones = getListOfZonesForCountry();
+                List<String> listOfSortZones = new ArrayList<String>(listOfZones);
+                listOfSortZones.sort(Comparator.<String, String>comparing(String::toString));
+                assertEquals(listOfZones,listOfSortZones);
+                driver.get(" http://localhost/litecart/admin/?app=countries&doc=countries");
+            }
+
+        }
+    }
+
+    private List<String> getListOfZonesForCountry() {
+        List<String>  listOfZones = new ArrayList<String>();
+        List<WebElement> webElementsofzones = driver.findElements(By.cssSelector("[type*=hidden][name *= name ]"));
+        for (WebElement webElement : webElementsofzones) {
+            String name = webElement.getAttribute("value");
+            listOfZones.add(name);
+        }
+        return listOfZones;
     }
 
 
@@ -29,7 +53,7 @@ public class Task9SortOfCountryAndGeofence extends TestBase {
         List<String> listOfCountries = new ArrayList<String>();
         List<WebElement> webElements = driver.findElements(By.cssSelector("tr.row"));
         for (WebElement webElement : webElements) {
-            String name = webElement.findElements(By.cssSelector("td")).get(4).getText();
+            String name = webElement.findElement(By.cssSelector("a")).getText();
             listOfCountries.add(name);
         }
         return listOfCountries;
