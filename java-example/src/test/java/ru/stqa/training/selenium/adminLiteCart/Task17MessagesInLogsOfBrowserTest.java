48624c5407af3eced4aa349462520bddf1559271
vi26 <44754162+vi26@users.noreply.github.com>
2018-11-27 01:53:00 +0300

    Выполнение задания 17 попытка №2

--- java-example/src/test/java/ru/stqa/training/selenium/adminLiteCart/Task17MessagesInLogsOfBrowserTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/adminLiteCart/Task17MessagesInLogsOfBrowserTest.java
@@ -5,27 +5,24 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.logging.LogEntry;
 
-import javax.sound.midi.Soundbank;
 import java.util.List;
 
-public class Task17NoMessagesInLogsOfBrowserTest extends TestBase {
+public class Task17MessagesInLogsOfBrowserTest extends TestBase {
 
     @Test
-    public void testNoMessagesInLogsOfBrowser() {
+    public void testMessagesInLogsOfBrowser() {
         driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1");
         List<WebElement> listOfProduct = driver.findElements(By.cssSelector(".dataTable [href *= product]:nth-child(2)"));
 
         for (int i = 0; i < listOfProduct.size(); i++) {
             driver.findElements(By.cssSelector(".dataTable [href *= product]:nth-child(2)")).get(i).click();
             for (LogEntry l : driver.manage().logs().get("browser").getAll()) {
-                System.out.println("Логи при открытии страницы с товаром №" + String.valueOf(i+1) );
+                System.out.println("Логи при открытии страницы с товаром №" + String.valueOf(i + 1));
                 System.out.println(l);
             }
             driver.navigate().back();
 
         }
-
-
 
 
     }
